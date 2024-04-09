56a1e574edb4f615a161cd4e4e25108c9b6f93e8
toskuef <96075802+toskuef@users.noreply.github.com>
2024-04-09 11:45:32 +0300

    Добавлен тест апи

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-      VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      }
-    })
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Kraft',
+                short_name: 'Kraft',
+                description: 'ERP система (PWA версия)',
+                theme_color: '#f8f9fa',
+            },
+            devOptions: {
+                enabled: true
+            }
+        })
 
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
